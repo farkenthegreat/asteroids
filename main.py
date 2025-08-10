8d89497e7b59b1f9602ca35c30325b9a3dbc5ab3
farkenthegreat <numberone@farken.co.uk>
2025-08-10 16:33:45 +0100

    CH2 L6

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import player
 
 def main():
     print("Starting Asteroids!")
@@ -8,12 +9,14 @@ def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
+    player_1 = player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player_1.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
